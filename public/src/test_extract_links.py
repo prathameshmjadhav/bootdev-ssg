5b53c7f4c2a3f8a6ccf74d4b2e75e461a20c89a4
Prathamesh <prathameshmjadhav@gmail.com>
2026-08-08 13:01:32 +0530

    Extract links from text using regex

---
+++ public/src/test_extract_links.py
@@ -0,0 +1,27 @@
+import unittest
+
+from extract_links import extract_markdown_images, extract_markdown_links
+
+
+class Test_Extract_Links(unittest.TestCase):
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        self.assertListEqual(
+            [
+                ("to boot dev", "https://www.boot.dev"),
+                ("to youtube", "https://www.youtube.com/@bootdotdev"),
+            ],
+            matches,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
